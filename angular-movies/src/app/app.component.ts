bb5804bffe0583cae49e4700adb5e68b0a75ffd6
Robert Gleason <l2ob122@gmail.com>
2025-01-29 16:42:56 -0800

    Showed off the new way doing for, showed off ng for ( the old way )

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  movie = {
-    title: 'Spider-Man',
-    releaseDate: new Date(),
-    price: 316.99,
-  };
+  movies = [
+    {
+      title: 'Spider-Man',
+      releaseDate: new Date(),
+      price: 316.99,
+    },
+    {
+      title: 'Moana',
+      releaseDate: new Date('2016-05-03'),
+      price: 23.99,
+    },
+  ];
 }
