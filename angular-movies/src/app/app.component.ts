340728587a94caf71241d1e6dbac875f9317c2e7
Robert Gleason <l2ob122@gmail.com>
2025-01-29 16:34:45 -0800

    Showed off 3 different pipes, currency,date,and uppercase

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -11,5 +12,6 @@ export class AppComponent {
   movie = {
     title: 'Spider-Man',
     releaseDate: new Date(),
+    price: 316.99,
   };
 }
