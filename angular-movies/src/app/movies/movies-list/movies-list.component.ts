829a431077f63b56fce2f112b5c1b7529d14ee3a
Robert Gleason <l2ob122@gmail.com>
2025-01-29 20:04:26 -0800

    showed how events work, creating 2 diffrent functions for adding and removing a movie

---
+++ angular-movies/src/app/movies/movies-list/movies-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import {
+  CurrencyPipe,
+  DatePipe,
+  NgOptimizedImage,
+  UpperCasePipe,
+} from '@angular/common';
+
+@Component({
+  selector: 'app-movies-list',
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgOptimizedImage],
+
+  templateUrl: './movies-list.component.html',
+  styleUrl: './movies-list.component.css',
+})
+export class MoviesListComponent {
+  @Input({ required: true })
+  movies!: any[];
+
+  addMovie() {
+    this.movies.push({
+      title: 'Die Hard',
+      releaseDate: new Date('1989-01-02'),
+      price: 680.99,
+    });
+  }
+
+  removeMovie(movie: any) {
+    let index = this.movies?.findIndex(
+      (movie: any) => movie.title === movie.title,
+    );
+    this.movies.splice(index!, 1);
+  }
+}
